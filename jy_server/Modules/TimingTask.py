b1ac728f439227809c501586dd7dbeee187fae3e
hua <858239977@qq.com>
2020-12-17 22:23:57 +0800

    1.1

---
+++ jy_server/Modules/TimingTask.py
@@ -0,0 +1,44 @@
+import time
+
+import requests
+from test_server.data.sql import sql_select
+from test_server.utils.responseJSON import responseJSON_1, responseJSON_0, responseJSON
+
+
+def timingTask(time_sleep):
+    url = "http://app.juyuanpark.com//api/user.ashx"
+    getMaxId = sql_select('SELECT MAX(id) FROM biz_user')[0][0]
+    while True:
+        time.sleep(time_sleep)
+        sql = '''
+            SELECT
+             TOP 1 invite_code ,id ,sex,token 
+            FROM
+             [dbo].[biz_user] 
+            WHERE
+             ( SELECT PATINDEX( '%[^0-9]%', invite_code ) ) = '0'
+              AND LEN(invite_code) =16  AND id > {} ORDER BY id DESC;
+        '''.format(getMaxId)
+        new_invite_code = sql_select(sql)
+        if new_invite_code == []:
+            print('没有新的用户进入！')
+            continue
+        invite_code = new_invite_code[0][0]
+        user_id = new_invite_code[0][1]
+        sex = new_invite_code[0][2]
+        token = new_invite_code[0][3]
+        getMaxId = user_id
+        print('用户invite_code', invite_code)
+        print('用户user_id', user_id)
+        print('用户sex', sex)
+        print('用户token', token)
+        payload = 'target=invite_code&user_id={}&invite_code={}&token={}'.format(user_id, invite_code, token)
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+
+# if __name__ == '__main__':
+#     timingTask(10)
