384e536bb5b25ef7791ac7bd543c0a5f71bd52c3
xiaoawuu <858239977@qq.com>
2021-05-09 00:25:30 +0800

    Merge branch 'master' of C:\test_server\test with conflicts.

--- jy_server/Modules/TimingTask.py
+++ jy_server/Modules/TimingTask.py
@@ -6,7 +6,8 @@ from test_server.utils.responseJSON import responseJSON_1, responseJSON_0, respo
 
 
 def timingTask(time_sleep):
-    url = "http://app.juyuanpark.com//api/user.ashx"
+    my_invite_code = '7D32EFAA378F96A3'
+    url = "http://app.juyuanpark.com/api/user.ashx"
     getMaxId = sql_select('SELECT MAX(id) FROM biz_user')[0][0]
     while True:
         time.sleep(time_sleep)
@@ -17,46 +18,36 @@ def timingTask(time_sleep):
              [dbo].[biz_user] 
             WHERE
              ( SELECT PATINDEX( '%[^0-9]%', invite_code ) ) = '0'
-              AND LEN(invite_code) =16  AND id > {} ORDER BY id DESC;
+              AND LEN(invite_code) =16  AND id > {} and  ORDER BY id DESC;
         '''.format(getMaxId)
         new_invite_code = sql_select(sql)
-        if not new_invite_code:
+        if new_invite_code == []:
             print('没有新的用户进入！')
             continue
-        invite_code = '7D32EFAA378F96A3'
+        invite_code = new_invite_code[0][0]
         user_id = new_invite_code[0][1]
         sex = new_invite_code[0][2]
         token = new_invite_code[0][3]
-
+        getMaxId = user_id
         print('用户invite_code', invite_code)
         print('用户user_id', user_id)
         print('用户sex', sex)
         print('用户token', token)
+        payload = 'target=invite_code&user_id={}&invite_code={}&token={}'.format(user_id, my_invite_code, token)
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
-        if sex == 1:  # 男用户
-            print('男性用户注册！')
-            is_recharge_record = sql_select('SELECT * FROM biz_order WHERE user_id = {} and status=1;'.format(user_id))
-            if not is_recharge_record:
-                print('已充值')
-                continue
-            payload = 'target=invite_code&user_id={}&invite_code={}&token={}'.format(user_id, invite_code, token)
-            print(payload)
-            response = requests.request("POST", url, headers=headers, data=payload)
-            print(response.text)
-            getMaxId = user_id
-        elif sex == 2:  # 女性用户
-            print('女性用户注册！')
-            is_true = sql_select('SELECT * FROM biz_user WHERE is_true = 1 and id = {};'.format(user_id))
-            if not is_true:
-                print('未认证')
-                continue
-            url = "http://app.juyuanpark.com//api/invitecode.ashx"
-            payload = 'target=modify_invite_code&user_id={}&invite_code={}&token={}'.format(user_id, invite_code, token)
-            print(payload)
-            response = requests.request("POST", url, headers=headers, data=payload)
-            print(response.text)
-            getMaxId = user_id
-if __name__ == '__main__':
-    timingTask(10)
+        print(payload)
+        # response = requests.request("POST", url, headers=headers, data=payload)
+        # return response
+
+# if __name__ == '__main__':
+#     timingTask(10)
+payload = 'target=invite_code&user_id=140088&invite_code=7D32EFAA378F96A3&token=98cec39350b74504a6bba30799e0dac5'
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
+print(payload)
+url = "http://app.juyuanpark.com//api/user.ashx"
+response = requests.request("POST", url, headers=headers, data=payload)
+print(response)
